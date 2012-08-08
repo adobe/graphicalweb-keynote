8d1c0a419679233212133af85089f55223db416c
cjgammon <cj.gammon@gmail.com>
2012-08-07 23:55:45 -0700

    add audio for blend modes and shader

--- src/js/graphicalweb/views/Section7_BLEND.js
+++ src/js/graphicalweb/views/Section7_BLEND.js
@@ -1,31 +1,44 @@
+/*global define $*/
 define(['graphicalweb/events/StateEvent',
+        'graphicalweb/events/UserEvent',
         'graphicalweb/controllers/CameraController',
+        'graphicalweb/controllers/AudioController',
+        'graphicalweb/models/VarsModel',
         'graphicalweb/views/components/CharBlend',
         'graphicalweb/views/components/Div',
         'graphicalweb/views/components/Scenery'],
 
-	function (StateEvent, Camera, Character, Div, Scenery) {
+	function (StateEvent, UserEvent, Camera, Audio, VarsModel, Character, Div, Scenery) {
 		
 		var Section6_BLEND = function () {
 			var instance = this,
                 stateId = 7,
                 character,
                 $cover,
-                $view;
+                $blockquotes,
+                view;
 
             instance.phaselength = 0;
             instance.phase = 0;
 
 //private
+
             function handle_animIn_COMPLETE() {
                 StateEvent.SECTION_ANIM_IN_COMPLETE.dispatch(stateId);
                 character.fadeIn();
-            }
 
+                if (VarsModel.PRESENTATION === true) {
+                    instance.next();
+                }
+            }
             
 //public
             instance.init = function () {
+                view = '.section7';
+                $blockquotes = $('blockquote' + view);
+
                 instance.phase = 0;
+                instance.phaselength = $blockquotes.length;
                 
                 character = new Character();
 
@@ -53,9 +66,52 @@ define(['graphicalweb/events/StateEvent',
 
            
             instance.next = function () {
+                var $currentQuote = $($blockquotes[instance.phase]);
+                
+                $blockquotes.fadeOut();
+                
+                switch (instance.phase) {
+                case 0:
+                    //amidead
+                    StateEvent.AUTOMATING.dispatch();         
+                    Div.setFace('talk');                   
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        Div.setFace('happy');
+                    });
+                    break;
+                case 1:
+                    //dont be afraid
+                    Div.setFace('happy');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                case 2:
+                    //take that
+                    Div.setFace('happy');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        StateEvent.WAIT_FOR_INTERACTION.dispatch();
+                    });
+                    break;
+                case 3:
+                    //distortion?
+                    StateEvent.AUTOMATING.dispatch();         
+                    Div.setFace('talk');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        Div.setFace('happy');
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                case 4:
+                    //princess another castle
+                    Div.setFace('happy');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                }
+                
                 instance.phase += 1;
-
-                //TODO:: sequence through
             };
 
             instance.stop = function () {
--- src/js/graphicalweb/views/Section8_SHADER.js
+++ src/js/graphicalweb/views/Section8_SHADER.js
@@ -1,19 +1,23 @@
 /*global define $ TWEEN*/
 
 define(['graphicalweb/events/StateEvent',
+        'graphicalweb/events/UserEvent',
         'graphicalweb/controllers/CameraController',
+        'graphicalweb/controllers/AudioController',
+        'graphicalweb/models/VarsModel',
         'graphicalweb/views/components/CharSvg',
         'graphicalweb/views/components/Div',
         'graphicalweb/views/components/Scenery'],
 
-	function (StateEvent, Camera, Character, Div, Scenery) {
+	function (StateEvent, UserEvent, Camera, Audio, VarsModel, Character, Div, Scenery) {
 		
-		var Section4_3D = function () {
+		var Section8_SHADER = function () {
 			var instance = this,
                 stateId = 8,
                 character,
                 $cover,
-                $view;
+                $blockquotes,
+                view;
 
             instance.phaselength = 0;
             instance.phase = 0;
@@ -21,12 +25,20 @@ define(['graphicalweb/events/StateEvent',
 //private
             function handle_animIn_COMPLETE() {
                 StateEvent.SECTION_ANIM_IN_COMPLETE.dispatch(stateId);
+                
+                if (VarsModel.PRESENTATION === true) {
+                    instance.next();
+                }
             }          
-            
             
 //public
             instance.init = function (direct) {
+                view = '.section8';
+                $blockquotes = $('blockquote' + view);
+
                 instance.phase = 0;
+                instance.phaselength = $blockquotes.length;
+
                 StateEvent.SECTION_READY.dispatch(stateId);
             };
 
@@ -48,9 +60,58 @@ define(['graphicalweb/events/StateEvent',
             };
 
             instance.next = function () {
+                var $currentQuote = $($blockquotes[instance.phase]);
+                
+                $blockquotes.fadeOut();
+                
+                switch (instance.phase) {
+                case 0:
+                    //welcome
+                    Div.setFace('happy');                   
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                case 1:
+                    //what is this?
+                    Div.setFace('talk');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        Div.setFace('happy');
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                case 2:
+                    //all together
+                    Div.setFace('happy');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        StateEvent.WAIT_FOR_INTERACTION.dispatch();
+                    });
+                    break;
+                case 3:
+                    //explore graphical web
+                    StateEvent.AUTOMATING.dispatch();         
+                    Div.setFace('happy');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                case 4:
+                    //this is what i'm talking about
+                    Div.setFace('talk');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                case 5:
+                    //let's get creative
+                    Div.setFace('talk');
+                    Audio.playDialogue($currentQuote.data('audio'), function () {
+                        Div.setFace('happy');
+                        UserEvent.NEXT.dispatch();
+                    });
+                    break;
+                }
                 instance.phase += 1;
-
-                //TODO:: sequence through
             };
 
             instance.stop = function () {
@@ -62,5 +123,5 @@ define(['graphicalweb/events/StateEvent',
             };
 		};
 
-		return new Section4_3D();
+		return new Section8_SHADER();
     });
