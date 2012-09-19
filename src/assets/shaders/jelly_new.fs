59a968318b62b668870f6389ed1588cb2160572d
cjgammon <cj.gammon@gmail.com>
2012-09-18 17:36:13 -0700

    add new shaders for canary

---
+++ src/assets/shaders/jelly_new.fs
@@ -0,0 +1,17 @@
+
+precision mediump float;
+
+varying vec2 v_texCoord;
+varying float shadow;
+uniform float backface;
+
+void main()
+{
+	if (gl_FrontFacing && backface == 1.0)
+		discard;
+
+	if (!gl_FrontFacing && backface == 0.0)
+		discard;
+
+    css_MixColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
